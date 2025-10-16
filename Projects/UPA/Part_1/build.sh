49ac8b014d283e421f3de85f929c743dcb80b125
xkaska02 <xkaska02@fit.vutbr.cz>
2025-10-16 17:47:20 +0200

    changed location of scripts

--- Projects/UPA/Part_1/build.sh
+++ Projects/UPA/Part_1/build.sh
@@ -2,5 +2,4 @@
 
 python3 -m venv Part_1
 source Part_1/bin/activate
-cd Part_1
 python3 -m pip install -r requirements.txt
--- Projects/UPA/Part_1/run.sh
+++ Projects/UPA/Part_1/run.sh
@@ -2,8 +2,6 @@
 
 source Part_1/bin/activate
 
-cd Part_1/
-
-python3 URL_Fetcher.py > url_test.txt
+python3 fetcher.py > url_test.txt
 
 head -n 10 url_test.txt | python3 scraper.py
