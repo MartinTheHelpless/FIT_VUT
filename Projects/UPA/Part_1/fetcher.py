0e22fc7cc053a45dea86146ce22058d6c1afa6a9
xkaska02 <xkaska02@fit.vutbr.cz>
2025-10-19 12:42:25 +0200

    changed everything back to otomoto.pl

--- Projects/UPA/Part_1/fetcher.py
+++ Projects/UPA/Part_1/fetcher.py
@@ -1,7 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
 import re
+from sys import stderr
 
 def go_to_site(url):
     headers = {
@@ -14,22 +14,23 @@ def go_to_site(url):
         with urllib.request.urlopen(req) as response:
             html = response.read().decode("utf-8", errors="ignore")
     except Exception as e:
-        print("Failed:", url, e)
+        print("Failed:", url, e, file=stderr)
         return []
 
     soup = BeautifulSoup(html, "html.parser")
-    products = [a["href"] for a in soup.find_all("a", attrs=re.compile("product--title"))]
+    products = [a["href"] for a in soup.find_all("a", href=re.compile("oferta"))]
     
-    return products
+    return set(products)
 
 def main():
-    base_url = "https://www.kindermaxx.de/en/toys/open-ended-play"
-    page_count = 7 # how many to load (when i tested it 7 was maximum)
+    base_url = "https://www.otomoto.pl/osobowe?search%5Border%5D=relevance_web"
+    page_count = 6
     all_products = []
     for i in range(page_count):
-        final_url = base_url+f"?p={i+1}"
+        final_url = base_url+f"&page={i+1}"
         all_products += go_to_site(final_url)
 
+    all_products = set(all_products)
     for product_link in all_products:
         print(product_link)
 
--- Projects/UPA/Part_1/scraper.py
+++ Projects/UPA/Part_1/scraper.py
@@ -3,6 +3,17 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 
+def get_attr_value(soup, div_id) -> str:
+    """returns value of the attribute or empty string if the attribute is not found
+
+    Args:
+        soup (BeautifulSoup): object with html to search in
+        div_id (str): id of the data-testid attribute on the site
+    """
+    div = soup.find("div", attrs={"data-testid":div_id})
+    attr_value = div.find("p", class_="ekybgc111 ooa-ugve4x").text if div else ""
+    return attr_value
+
 def get_product_info(url):
     """Function description"""
     headers = {
@@ -15,14 +26,26 @@ def get_product_info(url):
         with urllib.request.urlopen(req) as response:
             html = response.read().decode("utf-8", errors="ignore")
     except Exception as e:
-        print("Failed:", url, e)
+        print("Failed:", url, e, file=sys.stderr)
         return []
 
     soup = BeautifulSoup(html, "html.parser")
-    product_name = soup.find("h1", attrs=re.compile("product--title")).text
-    product_price = soup.find("span", attrs=re.compile("price--content content--default")).text
+    product_name = soup.find("h1", class_="offer-title big-text eng3xoo2 ooa-ev3vab").text
+    product_price = soup.find("span", class_="offer-price__number").text
     
-    info_value = [td.text for td in soup.find_all("td", attrs=re.compile("product--properties-value"))]
+    info_value = []
+    car_brand = get_attr_value(soup, "make")
+    car_model = get_attr_value(soup, "model")
+    car_color = get_attr_value(soup, "color")
+    door_count = get_attr_value(soup, "door_count")
+    nr_seats = get_attr_value(soup, "nr_seats")
+    year = get_attr_value(soup,"year")
+    info_value.append(car_brand)
+    info_value.append(car_model)
+    info_value.append(car_color)
+    info_value.append(door_count)
+    info_value.append(nr_seats)
+    info_value.append(year)
     info_value.insert(0, product_price) # add price to start
     info_value.insert(0, product_name)
     info_value.insert(0, url)
